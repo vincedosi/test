7a1252927c79e7a8b71c0a8686d82fb8f732071f
vincedosi <103569716+vincedosi@users.noreply.github.com>
2022-04-13 14:44:24 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-pip install --upgrade matplotlib
+
 
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
