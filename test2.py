7f17851807fa4f3151f278f306cd6c9c30a77acb
vincedosi <103569716+vincedosi@users.noreply.github.com>
2022-04-13 18:24:31 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,12 +1,31 @@
+
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit-shap",
+    version="1.0.2",
+    author="Snehan Kekre",
+    author_email="snehan@streamlit.io",
+    description="Streamlit component for SHAP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snehankekre/streamlit-shap",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["shap >= 0.4.0"],
+)
+
 # PACKAGES
 
-pip install shap
 
-import shap
-
-from IPython.display import display
-
-import shap
 
 import pandas as pd
 import numpy as np
