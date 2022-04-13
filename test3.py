51b8c70a8b96cb0dba03a835a36dfa9669b09ac4
vincedosi <103569716+vincedosi@users.noreply.github.com>
2022-04-13 16:28:57 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import seaborn as sns
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
